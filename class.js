6f44b58ee0e3acc86a41dfe2db10985e1b180ef4
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-02-14 19:33:15 +0100

    fix and update

--- class.js
+++ class.js
@@ -1,63 +1,63 @@
+/* eslint-disable  max-classes-per-file */
 class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  
-  const book = document.querySelector('.books');
-  
-  class Display {
-    static getBooks() {
-      let books;
-      if (localStorage.getItem('books') == null) {
-        books = [];
-      } else {
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-      return books;
+}
+
+const book = document.querySelector('.books');
+
+class Display {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') == null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-  
-    static displayBooks() {
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Display.getBooks();
+    let display = '';
+    books.forEach((book, i) => {
+      display += `
+          <div class="booksAdded">
+          <p>"${book.title}" by ${book.author}</p>
+          <button class="removeBtn" onclick="Display.removeBook(${i})">Remove</button>
+          </div>`;
+    });
+    book.innerHTML = display;
+  }
+
+  static addBook() {
+    const titleInput = document.querySelector('#fname').value;
+    const authorInput = document.querySelector('#lname').value;
+    if (titleInput !== '' && authorInput !== '') {
+      const newBook = new Book(titleInput, authorInput);
       const books = Display.getBooks();
-      let display = '';
-      books.forEach((book, i) => {
-        display += `
-            <div class="Add-book">
-            <p>"${book.title}" by ${book.author}</p>
-            <button class="removeBtn" onclick="Display.removeBook(${i})">Remove</button>
-            </div>`;
-      });
-      book.innerHTML = display;
-    }
-  
-    static addBook() {
-      const titleInput = document.querySelector('#first-name').value;
-      const authorInput = document.querySelector('#last-name').value;
-      if (titleInput !== '' && authorInput !== '') {
-        const newBook = new Book(titleInput, authorInput);
-        const books = Display.getBooks();
-        books.push(newBook);
-        localStorage.setItem('books', JSON.stringify(books));
-        this.displayBooks();
-      }
-    }
-  
-    static removeBook(id) {
-      const books = Display.getBooks();
-      const bookIndex = books.findIndex((item, i) => i === id);
-      books.splice(bookIndex, 1);
+      books.push(newBook);
       localStorage.setItem('books', JSON.stringify(books));
       this.displayBooks();
     }
   }
-  window.addEventListener('DOMContentLoaded', () => {
-    Display.displayBooks();
-  });
-  const form = document.querySelector('.form');
-  form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    Display.addBook();
-    form.reset();
-  });
-  
+
+  static removeBook(id) {
+    const books = Display.getBooks();
+    const bookIndex = books.findIndex((item, i) => i === id);
+    books.splice(bookIndex, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+    this.displayBooks();
+  }
+}
+window.addEventListener('DOMContentLoaded', () => {
+  Display.displayBooks();
+});
+const form = document.querySelector('.form');
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  Display.addBook();
+  form.reset();
+});
